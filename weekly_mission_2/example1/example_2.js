1fa944fc74bd57d80c8c1d0648d59e10e1f6eee2
FranciscoJardon <javier.jar23rosas@gmail.com>
2022-05-02 13:29:17 -0500

    add example_2

--- weekly_mission_2/example1/example_2.js
+++ weekly_mission_2/example1/example_2.js
@@ -1,7 +1,7 @@
 // Ejemplo 2: Crear un objeto con propiedades
 const myObjetc2 = {
     name: "Javier",
-    age: 27
+    age: 24
   }
   console.log("Ejemplo 2: Crear un objeto con propiedades")
   console.log(myObjetc2)
